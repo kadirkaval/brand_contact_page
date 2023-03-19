a033e7553cd954d302a92a63a9143ac51806cddc
PC <kadirkvl@gmail.com>
2023-03-19 18:48:37 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/kent/brand_contact_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kent.brand_contact_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.brown,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage("assets/kahve.jpg"),
+              radius: 100,
+            ),
+            Text(
+              "Flutter Kahvecisi",
+              style: TextStyle(
+                  color: Colors.brown[900],
+                  fontSize: 45,
+                  fontFamily: 'Pacifico-Regular'),
+            ),
+            const Text("BİR FİNCAN UZAĞINIZDA",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            Container(width: 150, height: 2, color: Colors.white70),
+            const SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 300,
+              height: 50,
+              decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(5),),
+                color:Colors.white70),
+              child: Row(children: const [
+                Icon(Icons.local_post_office),
+                SizedBox(width: 20),
+                Text("siparis@fkahvecisi.com", style: TextStyle(fontSize: 20),),
+              ],),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 300,
+              height: 50,
+              decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(5),),
+              color:Colors.white70),
+              child: Row(children: const [
+                Icon(Icons.phone),
+                SizedBox(width: 20),
+                Text("+90 555 55 55", style: TextStyle(fontSize: 20),),
+              ],),
+            ),
+          ],
+        ),
+      ),
+    ),
+  ));
+}
